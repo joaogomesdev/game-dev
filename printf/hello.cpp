4113c78096764ed8b37ab06a9442ae857bcd32b3
João Gomes <joaopfg.2002@gmail.com>
2022-12-14 21:12:57 +0000

    Rename 01 project | Finish Sectiotn 1 :tada:

---
+++ printf/hello.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main()
+{
+  printf("Hello Game Dev Jouney, im joaogomesdev");
+  printf("Im a 20 years old developer from PT");
+  
+  return 0;
+}
--- project/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main()
-{
-  printf("Hello Game Dev Jouney, im joaogomesdev. \n");
-  printf("Im a 20 years old developer from PT. \n");
-
-  return 0;
-}
